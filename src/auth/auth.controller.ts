e75b4b0ad4b74ec6766570d6024e24e4e54c3552
Himanshu Kataria <katariahk03@gmail.com>
2025-05-25 14:26:33 +0530

    Login add

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,9 @@ export class AuthController {
     @Post('register')
     register(@Body() dto: CreateUserDto){
         return this.authService.register(dto);
+    }
+    @Post('login')
+    login(@Body() dto: LoginUserDto) {
+        return this.authService.login(dto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from './dto/login-user.dto';
 @Injectable()
 export class AuthService {
   constructor(
@@ -27,6 +28,22 @@ export class AuthService {
     })
     const token = await this.jwtService.signAsync({userId: user.id});
     return {
+        access_token: token,
+    }
+  }
+  async login(dto: LoginUserDto){
+    const user = await this.prisma.user.findUnique({
+        where: {email: dto.email}
+    })
+    if(!user){
+        throw new UnauthorizedException('Invalid credentials');
+    }
+    const isPasswordValid = await bcrypt.compare(dto.password, user.password);
+    if(!isPasswordValid){
+        throw new UnauthorizedException('Invalid credentials');
+    }
+    const token = await this.jwtService.signAsync({userId: user.id});
+    return{
         access_token: token,
     }
   }
